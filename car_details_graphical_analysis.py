f609ca2534eff30e681b7497efb64006c259b8cd
Samarjit41 <125326957+Samarjit41@users.noreply.github.com>
2023-02-14 07:58:31 +0530

    Add files via upload

--- car_details_graphical_analysis.py
+++ car_details_graphical_analysis.py
@@ -2,17 +2,7 @@ import pandas as pd # data preprocessing
 import numpy as np #mathematical computation
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split # to split data into tain and split
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge,Lasso
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
-import pickle
+
 print("=================================loading the data sheet and preprocessing===============================")
 df = pd.read_csv('C:\Capstone project\CAR DETAILS.csv')
 print(df.shape) # rows=4340,cols=8
